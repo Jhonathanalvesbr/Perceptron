1b97fb0a3a8e9719785d45e245703e6620ab1f0e
Jhonathanalvesbr <Jhonathanalves_br@hotmail.com>
2020-06-03 10:57:19 -0300

    Update Rede.java

--- src/perceptron/Rede.java
+++ src/perceptron/Rede.java
@@ -1,17 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package perceptron;
 
-/**
- *
- * @author Administrador
- */
 public class Rede {
 
-    int[][] entrada = {{0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1},
+    int qntLinha = 10;
+    int qntColuna = 4;
+    int qntEntrada = 16;
+    int qntNeuronio = 4;
+
+    int[][] entrada = {
+    {0, 1, 0, 1, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 0, 1},
     {0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1},
     {0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 1, 0, 1, 1, 1, 1},
     {1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1},
@@ -20,29 +17,31 @@ public class Rede {
     {1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1},
     {1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1},
     {1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1},
-    {1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1}};
+    {1, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1},
+    {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}};
 
     int[][] target = {
-        {1, 1, 1},
-        {1, 1, 0},
-        {1, 1, -1},
-        {1, 0, 1},
-        {1, 0, 0},
-        {1, 0, -1},
-        {1, -1, -1},
-        {1, -1, 0},
-        {1, -1, 1},
-        {0, 1, 1}};
+    {1,1,1,1},
+    {1,1,1,-1},
+    {1,1,-1,1},
+    {1,1,-1,-1},
+    {1,-1,1,1},
+    {1,-1,1,-1},
+    {1,-1,-1,1},
+    {1,-1,-1,-1},
+    {-1,1,1,1},
+    {-1,1,1,-1}};
 
-    int[][] f = new int[15][3];
-    Neuronio neuronio[] = new Neuronio[3];
+    int[][] f = new int[qntLinha][qntColuna];
+    Neuronio neuronio[] = new Neuronio[qntNeuronio];
     int limiar, taxaAprendizado, epocas = 0;
 
     public Rede(int limiar, int taxaAprendizado) {
         this.limiar = limiar;
         this.taxaAprendizado = taxaAprendizado;
-        for (int i = 0; i < 3; i++) {
-            neuronio[i] = new Neuronio(entrada, 4, 1);
+        for (int i = 0; i < qntNeuronio; i++) {
+            neuronio[i] = new Neuronio(entrada, 4, 1, qntLinha, qntColuna, qntEntrada);
         }
     }
 
@@ -51,25 +50,25 @@ public class Rede {
         boolean varia;
         do {
             varia = false;
-            for (int i = 0; i < 10; i++) {
-                neuronio[0].somatorio(i);
-                neuronio[1].somatorio(i);
-                neuronio[2].somatorio(i);
-                f[i][0] = neuronio[0].limiar();
-                f[i][1] = neuronio[1].limiar();
-                f[i][2] = neuronio[2].limiar();
-                for (int k = 0; k < 3; k++) {
+            for (int i = 0; i < qntLinha; i++) {
+                for (int j = 0; j < qntNeuronio; j++) {
+                    neuronio[j].somatorio(i);
+                }
+                for (int j = 0; j < qntNeuronio; j++) {
+                    f[i][j] = neuronio[j].limiar();
+                }
+                for (int k = 0; k < qntColuna; k++) {
                     if (f[i][k] != target[i][k]) {
                         //neuronio[k].atualizaPesos(i, target[i][k], f[i][k]);
                         varia = true;
                     }
                 }
                 if (varia) {
-                    neuronio[0].atualizaPesos(i, target[i][0], f[i][0]);
-                    neuronio[1].atualizaPesos(i, target[i][1], f[i][1]);
-                    neuronio[2].atualizaPesos(i, target[i][2], f[i][2]);
+                    for (int j = 0; j < qntNeuronio; j++) {
+                        neuronio[j].atualizaPesos(i, target[i][j], f[i][j]);
+                    }
                 }
-                for (int j = 0; j < 3; j++) {
+                for (int j = 0; j < qntColuna; j++) {
                     System.out.print(f[i][j] + " ");
                 }
                 System.out.println("");
@@ -81,36 +80,16 @@ public class Rede {
     }
 
     public int[] teste(int entrada[]) {
-        neuronio[0].somatorio(entrada);
-        neuronio[1].somatorio(entrada);
-        neuronio[2].somatorio(entrada);
-        int[] res = new int[3];
-        res[0] = neuronio[0].limiar();
-        res[1] = neuronio[1].limiar();
-        res[2] = neuronio[2].limiar();
-        System.out.print(res[0]);
-        System.out.print(res[1]);
-        System.out.println(res[2]);
+        for (int i = 0; i < qntNeuronio; i++) {
+            neuronio[i].somatorio(entrada);
+        }
+        int[] res = new int[qntNeuronio];
+        System.out.print("Saida: ");
+        for (int i = 0; i < qntNeuronio; i++) {
+            res[i] = neuronio[i].limiar();
+            System.out.print(res[i]);
+        }
+        System.out.println("");
         return res;
     }
 }
-
-/*                for (int j = 0; j < 3; j++) {
- neuronio[j].somtorio(i);
- neuronio[j].fEnt[i] = neuronio[j].limiar();
- f[i][j] = neuronio[j].fEnt[i];
- }
- for (int j = 0; j < 3; j++) {
- if(target[i][j] != f[i][j]){
- for (int k = 0; k < 3; k++) {
- neuronio[k].atualizaPesos(i);
- }
- varia = true;
- }
- }
- for (int j = 0; j < 3; j++) {
- System.out.print(f[i][j]);
- }
- System.out.println("");
- }*/
-//}
