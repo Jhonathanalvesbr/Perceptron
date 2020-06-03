519c95ac8adb9d8c623c44bd83c8007c084d7250
Jhonathanalvesbr <Jhonathanalves_br@hotmail.com>
2020-06-03 10:55:50 -0300

    Update Neuronio.java

--- src/perceptron/Neuronio.java
+++ src/perceptron/Neuronio.java
@@ -7,30 +7,36 @@ public class Neuronio {
     int[][] entrada;
     double limiar;
     int taxaAprendizado;
-    int target[] = new int[10];
     boolean varia = false;
+    int qntLinha;
+    int qntColuna;
+    int qntEntrada;
 
-    public Neuronio(int[][] entrada, double limiar, int taxaAprendizado) {
-        peso = new double[16];
+    public Neuronio(int[][] entrada, double limiar, int taxaAprendizado, int qntLinha, int qntColuna, int qntEntrada) {
+        peso = new double[qntEntrada];
         this.entrada = entrada;
         this.limiar = limiar;
         this.taxaAprendizado = taxaAprendizado;
+        int target[] = new int[qntLinha];
+        this.qntLinha = qntLinha;
+        this.qntColuna = qntColuna;
+        this.qntEntrada = qntEntrada;
     }
 
     public void somatorio(int i) {
         somatorio = 0;
-        for (int j = 0; j < 16; j++) {
+        for (int j = 0; j < qntEntrada; j++) {
             somatorio = somatorio + (entrada[i][j] * peso[j]);
         }
     }
-    
+
     public void somatorio(int[] vetEntrada) {
         somatorio = 0;
-        for (int j = 0; j < 16; j++) {
+        for (int j = 0; j < qntEntrada; j++) {
             somatorio = somatorio + (vetEntrada[j] * peso[j]);
         }
     }
-    
+
     public int limiar() {
         if (somatorio > limiar) {
             return 1;
@@ -40,9 +46,9 @@ public class Neuronio {
             return 0;
         }
     }
-    
+
     public void atualizaPesos(int i, int target, int f) {
-        for (int j = 0; j < 16; j++) {
+        for (int j = 0; j < qntEntrada; j++) {
             peso[j] = peso[j] + taxaAprendizado * (target - f) * entrada[i][j];
         }
     }
