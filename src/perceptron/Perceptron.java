2ba368b776a14b58f2fd3669c97df4ddb3726b3e
Jhonathanalvesbr <Jhonathanalves_br@hotmail.com>
2020-06-03 10:56:23 -0300

    Update Perceptron.java

--- src/perceptron/Perceptron.java
+++ src/perceptron/Perceptron.java
@@ -2,11 +2,6 @@ package perceptron;
 
 import javax.swing.*;
 
-/* É importante destacar que devido ao fato da atualização dos pesos usar a equação
- w = w + taxa * (t - f) * x  , os valores usados para as saídas desejadas foram bipolares (1 e -1)
- e não binários (1 e 0). Isto se deve ao fato de que atribuir 0 para a saída faz com que (t - f) 
- sempre seja (0 - f) e isto dificulta o algoritmo de parar o ciclo de aprendizado.
- */
 public class Perceptron {
 
     public static void main(String[] args) {
